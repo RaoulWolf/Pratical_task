c7cc11c10163b0daafc4a2b2162046e00a4222a5
Raoul Wolf <raoul.wolf@ibv.uio.no>
2018-05-10 15:59:55 +0200

    A bit more Python magic and slightly updated R Markdown file

--- Scripts/01_Data_tidying.py
+++ Scripts/01_Data_tidying.py
@@ -8,69 +8,34 @@ Created on Thu May 10 10:37:00 2018
 
 # loading the necessary packages
 import numpy as np
-# import os
 import pandas as pd 
-#from numpy import nan
-#from pandas import read_excel
-# import xlrd as xl 
-# from pandas import ExcelWriter
-# from pandas import ExcelFile
 
-#from openpyxl import load_workbook
+# importing the Excel file
+data_raw = pd.read_excel("../Data/tidal_sample.xlsx", 
+                         dtype={'GPS Longitude': np.str, 'GPS Latitude': np.str})
 
-# cwd = os.getcwd()
-
-#data_raw = load_workbook(filename = "../Data/tidal_sample.xlsx")
-
-#print data_raw.get_sheet_names()
-
-data_raw = pd.read_excel("../Data/tidal_sample.xlsx", na_values='<NULL>', dtype={'GPS Longitude': np.str, 'GPS Latitude': np.str})
+data_raw.columns = ['Date', 'Time', 'Substrate Type', 'GPS Latitude', 'GPS Longitude', 'Depth', 'Comment', 'Kelp Percentage']
 
 
-#s = "123,456"
-#s = float(s.replace(',', '.'))
-
-#data_raw['depth'] = data_raw['depth'].replace(',', '.')
-
-#df.dropna(subset = ['column1_name', 'column2_name', 'column3_name'])
-
-
-#df = df[np.notnull(df['EPS'])]
-
-#data_raw['depth'] = [x.replace('<NULL>', 'nan') for x in data_raw['depth']]
-
-#data_raw = data_raw[pd.notnull(data_raw['depth'])]
-
-#data_raw = data_raw.dropna(subset = ['depth'])
-
-#df.dropna(subset = ['column1_name', 'column2_name', 'column3_name'])
-
-#data_raw = data_raw.dropna(subset = ['GPS Longitude', 'GPS Latitude', 'depth'])
-
-#data_raw['GPS Longitude'] = pd.to_string(data_raw['GPS Longitude'])
-
-data_raw['depth'] = [x.replace(',', '.') for x in data_raw['depth']]
+data_raw['Depth'] = [x.replace(',', '.') for x in data_raw['Depth']]
 data_raw['GPS Longitude'] = [x.replace(',', '.') for x in data_raw['GPS Longitude']]
 data_raw['GPS Latitude'] = [x.replace(',', '.') for x in data_raw['GPS Latitude']]
 
-data_raw['depth'] = pd.to_numeric(data_raw['depth'], errors = 'coerce')
+data_raw['Depth'] = pd.to_numeric(data_raw['Depth'], errors = 'coerce')
 data_raw['GPS Longitude'] = pd.to_numeric(data_raw['GPS Longitude'], errors = 'coerce')
 data_raw['GPS Latitude'] = pd.to_numeric(data_raw['GPS Latitude'], errors = 'coerce')
+data_raw['Kelp Percentage'] = pd.to_numeric(data_raw['Kelp Percentage'], errors = 'coerce')
 
-print(data_raw)
+data_raw = data_raw.dropna(subset = ['GPS Longitude', 'GPS Latitude', 'Depth'])
 
-#df['a'] = [x.replace(',', '.') for x in df['a']]
+data_raw['Date'] = pd.to_datetime(data_raw['Date'])
+#data_raw['Date'].to_string()
+#data_raw['Date'].str.split(' ')
+#data_raw['Date'] = np.str(data_raw['Date'])
+#data_raw['Date'] = data_raw['Date'].str.slice(0, 9)
 
-#df['a'] = df['a'].astype(float)
+#print(data_raw)
 
-#data_raw.head()
+print(data_raw.describe())
 
-#>>> a = [['a', '1.2', '4.2'], ['b', '70', '0.03'], ['x', '5', '0']]
-#>>> df = pd.DataFrame(a, columns=['col1','col2','col3'])
-#>>> df
 
-#df[['col2','col3']] = df[['col2','col3']].apply(pd.to_numeric)
-
-#data_raw[['GPS Longitude', 'GPS Latitude', 'depth']] = data_raw[['GPS Longitude', 'GPS Latitude', 'depth']].apply(pd.to_numeric)
-
-#data_raw.add_format({'num_format': '####,00'}) 
